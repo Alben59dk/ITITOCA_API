737ff9cf98b85be6a4cabf98ad036077f4799a33
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-06-14 16:41:24 +0200

    possibility to publish or archive a content

---
+++ controllers/content.js
@@ -0,0 +1,58 @@
+let ArticleModel = require ('../models/article')
+
+class ContentController {
+
+  static findAll(res) {
+    ArticleModel.find({}, (err, result) => {
+      if (err) {
+        res.status(503).json({
+          error: err.message
+        })
+        return
+      }
+      if (result) {
+        res.status(200).json(result)
+      } else {
+        res.status(200).json([])
+      }
+    })
+  }
+
+  static publishOneContent(id, res) {
+    ArticleModel.findByIdAndUpdate(id, {
+      status: 'PUBLISHED'
+    }, { new: true }, (err, doc) => {
+      if (err) {
+        res.status(503).json({
+          error: err.message
+        })
+        return
+      }
+      if (doc) {
+        res.status(200).json(doc)
+      } else {
+        res.status(204).json({})
+      }
+    })
+  }
+
+  static archiveOneContent(id, res) {
+    ArticleModel.findByIdAndUpdate(id, {
+      status: 'ARCHIVED'
+    }, { new: true }, (err, doc) => {
+      if (err) {
+        res.status(503).json({
+          error: err.message
+        })
+        return
+      }
+      if (doc) {
+        res.status(200).json(doc)
+      } else {
+        res.status(204).json({})
+      }
+    })
+  }
+}
+
+module.exports = ContentController
