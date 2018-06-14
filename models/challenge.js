b6f8002055ba1a8ce0b44a1f6ffcb823b95eedbf
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-06-14 11:56:00 +0200

    challenge model discriminated from article model

---
+++ models/challenge.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose')
+let ArticleModel = require('./article.js')
+
+let challengeSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    default: 'CHALLENGE',
+    required: true
+  },
+  end_of_participation_date: {
+    type: Date,
+    required: true
+  },
+  synthesis: {
+    type: String
+  }
+})
+
+let ChallengeModel = ArticleModel.discriminator('ChallengeModel', challengeSchema)
+module.exports = ChallengeModel
