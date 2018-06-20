80d7540956cbec023a1d10a30af6878001d45ff7
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-06-20 17:10:58 +0200

    comments and id regex pour GET one route

--- routes/content.js
+++ routes/content.js
@@ -3,27 +3,27 @@ const ContentController = require('../controllers/content')
 const challengeRouter = require('./challenge')
 const articleRouter = require('./article')
 
-//ROUTERS
 const ContentRouter = express.Router()
 
 ContentRouter.use('/challenge', challengeRouter)
 ContentRouter.use('/article', articleRouter)
 
-/////////////////
-//    ROUTES   //
-/////////////////
+// Get all contents
 ContentRouter.get('/', (req, res) => {
   ContentController.findAll(req, res)
 })
 
- ContentRouter.get('/:id', (req, res) => {
+// Get one content by ID
+ContentRouter.get('/:id([a-f\\d]{24})', (req, res) => {
   ContentController.findOne(req.params.id, res)
 })
 
+// Publish one content by ID
 ContentRouter.post('/publish/:id', (req, res) => {
   ContentController.publishOne(req.params.id, res)
 })
 
+// Archive one content by ID
 ContentRouter.post('/archive/:id', (req, res) => {
   ContentController.archiveOne(req.params.id, res)
 })
