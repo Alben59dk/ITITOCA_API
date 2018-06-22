2be2dbf14b5ac8386e0ec66e950f2e80ef8bdf0e
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-06-22 17:53:00 +0200

    regexp verification for id on routes

--- routes/content.js
+++ routes/content.js
@@ -19,12 +19,12 @@ ContentRouter.get('/:id([a-f\\d]{24})', (req, res) => {
 })
 
 // Publish one content by ID
-ContentRouter.post('/publish/:id', (req, res) => {
+ContentRouter.post('/publish/:id([a-f\\d]{24})', (req, res) => {
   ContentController.publishOne(req.params.id, res)
 })
 
 // Archive one content by ID
-ContentRouter.post('/archive/:id', (req, res) => {
+ContentRouter.post('/archive/:id([a-f\\d]{24})', (req, res) => {
   ContentController.archiveOne(req.params.id, res)
 })
 
@@ -33,7 +33,7 @@ ContentRouter.get('/published', (req, res) => {
   ContentController.publishedOnes(req, res)
 })
 
-ContentRouter.get('/filter/:categoryId/:timeId*?', (req, res) => {
+ContentRouter.get('/filter/:categoryId([a-f\\d]{24})/:timeId([a-f\\d]{24})*?', (req, res) => {
   ContentController.findFiltered(req, res)
 })
 
