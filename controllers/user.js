d295b579602e0e7a1c4e6db4ac1f8f17b938cacd
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-06-24 16:36:13 +0200

    indentation

--- controllers/user.js
+++ controllers/user.js
@@ -5,10 +5,36 @@ const UserModel = require('../models/user')
 
 class UserController {
 
-    static findAll(res) {
-      UserModel.find({})
-      .exec(function (err, data) {
-        if(err) {
+  static findAll(res) {
+    UserModel.find({})
+    .exec(function (err, data) {
+      if(err) {
+        res.status(503).json({
+          error: err.message
+        })
+        return
+      }
+      if(data) {
+        res.status(200).json(data)
+      } else {
+        res.status(200).json([])
+      }
+    })
+  }
+
+  static login(req, res) {
+    UserModel.find({
+      email: req.body.email
+    })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth Failed : email not found"
+        })
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
           res.status(503).json({
             message: err
           })
@@ -19,54 +45,26 @@ class UserController {
             message: "Auth Failed : wrong password"
           })
         } else {
-          res.status(200).json([])
-        }
-      })
-    }
-
-    static login(req, res) {
-      UserModel.find({
-        email: req.body.email
-      })
-      .exec()
-      .then(user => {
-        if (user.length < 1) {
-          return res.status(401).json({
-            message: "Auth Failed : email not found"
+          const token = jwt.sign(
+              {
+                email: user[0].email,
+                userId: user[0]._id,
+                role: user[0].roles,
+              },
+              '1T1T0C4_S3CR3T'
+            )
+          res.status(200).json({
+              token: token
           })
         }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-          if (err) {
-            res.status(503).json({
-              message: err
-            })
-            return
-          }
-          if (!result) {
-            res.status(401).json({
-              message: "Auth Failed : wrong password"
-            })
-          } else {
-            const token = jwt.sign(
-                {
-                  email: user[0].email,
-                  userId: user[0]._id,
-                  role: user[0].roles,
-                },
-                '1T1T0C4_S3CR3T'
-              )
-            res.status(200).json({
-                token: token
-            })
-          }
+      })
+    })
+    .catch(err => {
+        res.status(503).json({
+          error: err.message
         })
-      })
-      .catch(err => {
-          res.status(503).json({
-            error: err.message
-          })
-      })
-    }
+    })
+  }
 
   static addOne(req, res) {
     let salt = bcrypt.genSaltSync(11)
@@ -88,77 +86,73 @@ class UserController {
     })
   }
 
-    static addAdmin(req, res) {
-      let salt = bcrypt.genSaltSync(11)
-      let hash = bcrypt.hashSync(req.body.password, salt)
-      let newUser = new UserModel({
-        email: req.body.email,
-        pseudo: req.body.pseudo,
-        password: hash,
-        roles: 'ADMIN'
-      })
-      newUser.save(function (err, user) {
-        if (err) {
-          res.status(503).json({
-            message: err.message
-          })
-        } else {
-            res.status(200).json(user)
-        }
-      })
-    }
-
-    static disableOne(id, res) {
-        UserModel.findByIdAndUpdate(id, {
-          active: false
-        }, { new:  true }, (err, doc) => {
-            if(err) {
-                res.status(503).json({
-                    error: err.message
-                })
-                return 
-            }
-            if (doc) {
-                res.status(201).json(doc)
-            } else {
-                res.status(204).json({})
-            }
+  static addAdmin(req, res) {
+    let salt = bcrypt.genSaltSync(11)
+    let hash = bcrypt.hashSync(req.body.password, salt)
+    let newUser = new UserModel({
+      email: req.body.email,
+      pseudo: req.body.pseudo,
+      password: hash,
+      roles: 'ADMIN'
+    })
+    newUser.save(function (err, user) {
+      if (err) {
+        res.status(503).json({
+          message: err.message
         })
-    }
+      } else {
+          res.status(200).json(user)
+      }
+    })
+  }
 
-    static activateOne(id, res) {
-        UserModel.findByIdAndUpdate(id, {
-            active: true
-        }, { new:  true }, (err, doc) => {
-            if(err) {
-                res.status(503).json({
-                    error: err.message
-                })
-                return 
-            }
-            if (doc) {
-                res.status(201).json(doc)
-            } else {
-                res.status(204).json({})
-            }
+  static disableOne(id, res) {
+    UserModel.findByIdAndUpdate(id, {
+      active: false
+    }, { new:  true }, (err, doc) => {
+      if(err) {
+        res.status(503).json({
+          error: err.message
         })
-    }
+        return
+      }
+      if (doc) {
+        res.status(201).json(doc)
+      } else {
+        res.status(204).json({})
+      }
+    })
+  }
 
-    static deleteOne(id, res) {
-        UserModel.findByIdAndDelete(id, (err, doc) => {
-          if (err) {
-            res.status(503).json({
-              error: err.message
-            })
-            return
-          }
-          if (doc) {
-            res.status(201).json(doc)
-          } else {
-            res.status(204).json({})
-          }
+  static activateOne(id, res) {
+    UserModel.findByIdAndUpdate(id, {
+      active: true
+    }, { new:  true }, (err, doc) => {
+      if(err) {
+        res.status(503).json({
+          error: err.message
         })
-    }
+        return
+      }
+      if (doc) {
+        res.status(201).json(doc)
+      } else {
+        res.status(204).json({})
+      }
+    })
+  }
+
+  static deleteOne(id, res) {
+    UserModel.findByIdAndDelete(id, (err) => {
+      if (err) {
+        res.status(503).json({
+          error: err.message
+        })
+        return
+      }
+      res.status(204).json({})
+    })
+  }
 }
 
 module.exports = UserController
